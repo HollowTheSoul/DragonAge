7da6ede7e0b439488f2bdcdd8c6555f78c8ed044
miharjat <miharjat@mcmaster.ca>
2017-10-20 13:00:56 -0400

    Added dragon and enemy data.

--- Dragon_Age/src/Dragon Age/gameData.py
+++ Dragon_Age/src/Dragon Age/gameData.py
@@ -1,11 +1,26 @@
-#window size
+import pygame
+
+#window and screen
 WINDOW_SIZE = width, height = 800, 620
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
 #game mode
 isIntro = True  
 isGameOver = False
 isPaused = True
 
+#game path data
+checkPoints = []
+
 #game statistics
 wave = 1
 life = 10
+
+#dragon data
+dragonSize = 30
+
+#enemy data
+enemySpeed = 4
+enemyCount = 30
+enemyMaxCount = 30
+enemyNum = 7 #number of enemies per wave
