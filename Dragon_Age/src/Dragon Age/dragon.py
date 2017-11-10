08cc5451f4fd04e8167d2fc280b09b3df87b0437
miharjat <miharjat@mcmaster.ca>
2017-11-10 18:02:34 -0500

    Added attack speed as maxCounter

--- Dragon_Age/src/Dragon Age/dragon.py
+++ Dragon_Age/src/Dragon Age/dragon.py
@@ -5,10 +5,11 @@ class Dragon(object):
     def __init__(self, dragon, dragonDatabase):
         tup = dragonDatabase[dragon]
         self.dragon = tup[0]
-        self.element = tup[1]
+        self.element = tup[1] 
         self.baseAttack = tup[2]
         self.baseHp = tup[3]
         self.upgrade = tup[4]
+        self.maxCounter = tup[6] #attack speed, the lower the faster the attack
         self.attackGrowth = 10
         self.bounds = None
         self.button = None
