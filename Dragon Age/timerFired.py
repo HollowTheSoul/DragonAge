8b6c3c1e4e5770eafb488ae80cdaaed5d44828c8
liuz23 <liuz23@mcmaster.ca>
2017-11-13 15:16:37 -0500

    fix indentation error when pushing

---
+++ Dragon Age/timerFired.py
@@ -0,0 +1,25 @@
+import pygame
+from timerBullet import moveAllBullets, removeBullets, setTarget, shootEnemies, setDamage, setBullets, allBulletsRemoved
+from timerHover import hover, buildTowerHover, gameoverHover
+from timerEnemy import moveAllEnemies, roundOver
+from enemy import setWave
+import gameData
+
+#-------------------------TimerFired functions----------------------------
+#initiate all the timer fired functions
+def timerFired():
+    if gameData.isGameOver:
+        gameoverHover()
+    elif gameData.isIntro == True:
+        hover()
+        moveAllEnemies()
+        setTarget()
+        setBullets()
+        moveAllBullets()
+        shootEnemies()
+        removeBullets()
+        #after a wave is cleared, check conditions and add the next wave
+        if (gameData.enemies != [] and roundOver() and allBulletsRemoved()):
+            gameData.enemies = []
+            gameData.wave += 1
+            setWave()
