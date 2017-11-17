27445bf4b3539cf9db9130aa300895d830a33c54
miharjat <miharjat@mcmaster.ca>
2017-11-16 21:22:16 -0500

    Added status function for draw drawStatus

--- Dragon_Age/src/Dragon Age/draw.py
+++ Dragon_Age/src/Dragon Age/draw.py
@@ -25,12 +25,25 @@ def drawTowers():#draw all towers on board
     for dragon in gameData.dragonParty:
         if dragon.onBoard == True:
             dragon.drawTower(gameData.screen)
-            
-def drawParty():
-    startY =60
-    startX = 690
-    width = 100
-    height = 25
+
+
+def drawStatus(): #draw the status of the tower selected
+    color = 255,255,255
+    font = pygame.font.Font("pokemon_pixel_font.ttf",20)
+    pygame.draw.rect(gameData.screen,color,(690,720,400,500),2)
+    if gameData.playerShowStatus!= None:
+        dragon = gameData.playerShowStatus
+        dragon.drawRadius(gameData.screen)
+
+        name = font.render(dragon.dragon,True,color)
+        gameData.screen.blit(name,(690,350))
+        level = font.render(("Level: %d" %dragon.upgrade),True,color)
+        gameData.screen.blit(level,(690,380))
+        attack = font.render(("Attack: %d"%dragon.baseAttack),True,color)
+        gameData.screen.blit(attack,(690,410))
+
+
+def drawGameStats(): #draw the money collected so far
     font = pygame.font.Font("pokemon_pixel_font.ttf",20)
     store = pygame.image.load("img/store.png")
     store = pygame.transform.scale(store, (35,35))
@@ -38,22 +51,39 @@ def drawParty():
     money = font.render(str(gameData.playerCoins),True,(255,255,255))
     text = "Money:"
     moneySymbol = font.render(text,True,(255,255,255))
+
+    #draw money
     gameData.screen.blit(money,(760,35))
     gameData.screen.blit(moneySymbol,(720,35))
+
+    #draw level
     level = font.render(("Level: %d" %gameData.wave),True,(255,255,255))
     gameData.screen.blit(level,(20,20))
+
+    #draw life
     life = font.render(("Remaining Life: %d" %gameData.life),True,(255,255,255))
     gameData.screen.blit(life,(560,470))
-    for i in range(len(gameData.dragonParty)):
-        dragon = gameData.dragonParty[i]#display name of each pokemon
+
+def drawParty():
+    startY = 120
+    startX = 690
+    width = 100
+    height = 25
+    font = pygame.font.Font("pokemon_pixel_font.ttf",20)
+    for i in range(len(gameData.dragonType)):
+        dragon = gameData.dragonType[i]#display name of each pokemon
         name = dragon.dragon
+
+        dragonImage = pygame.image.load("img/%s.png" % name)
+        dragonImage = pygame.transform.scale(dragonImage, (35,35))
+        gameData.screen.blit(dragonImage, (startX+10,startY-30))
+
         dragon.button = startX,startY,width,height
         if gameData.playerHover == dragon or gameData.playerSelected == dragon:
-            # pygame.draw.rect(gameData.screen,(255,0,0),(dragon.button),1)
-            dragon.drawRadius(gameData.screen)
+            pygame.draw.rect(gameData.screen,(255,0,0),(dragon.button),1)
         name = font.render(name,True,(255,255,255))
-        gameData.screen.blit(name,(startX+5,startY+5))
-        startY+=25
+        gameData.screen.blit(name,(startX+10,startY+5))
+        startY+=80
 
 def drawAllBullets():#draws all bullets on board
     for tower in gameData.dragonParty:
@@ -67,3 +97,5 @@ def drawAll():
     drawTowers()
     drawParty()
     drawAllBullets()
+    drawGameStats()
+    drawStatus()
