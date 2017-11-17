2b1899e4454d0721349db880a8e9d17138b7424c
miharjat <miharjat@mcmaster.ca>
2017-11-16 21:21:10 -0500

    Changed the mouse click functions.

--- Dragon_Age/src/Dragon Age/gameManager.py
+++ Dragon_Age/src/Dragon Age/gameManager.py
@@ -1,10 +1,11 @@
 import pygame
 from dragonTower import setDragons
 from enemy import setWave
-from path import createPath, inPlay, onBoard, inParty, upgradeBound
+from path import createPath, inPlay, onBoard, inParty, upgradeBound, inTowerBounds, canBuild
 from draw import drawAll
 from timerFired import timerFired
 import gameData
+import copy
 
 def gameInit():
     #Create path
@@ -30,25 +31,45 @@ def mousePress(x,y):
     if inPlay(x,y):
         gameData.isPaused = False
 
+    elif gameData.playerShowStatus!= None and upgradeBound(x,y):
+        print("Inside show status")
+        (gameData.playerShowStatus).upgradeTower()
+
+    #Click on party selection,
     elif inParty(x,y):
         curDragon = inParty(x,y)#current dragon
-        if curDragon.onBoard == False:#only in party not on board yet
-            gameData.playerSelected = curDragon#pick up pokemon
-            gameData.playerSelected.x,gameData.playerSelected.y = x,y
-        #already on board, show status
-        else: 
-            gameData.playerShowStatus = curDragon
-            print gameData.playerShowStatus
 
-    elif gameData.playerSelected!=None:
+        #build new tower if the tower is already on board
+        if (gameData.playerCoins >= 150): #only if the player have sufficient money
+            selectedDragon = copy.copy(curDragon) #create a copy to be appended later to the party
+            gameData.playerSelected = selectedDragon#pick up pokemon
+            gameData.playerSelected.x,gameData.playerSelected.y = x,y
+        
+        #No money
+        else:
+            font = pygame.font.Font("pokemon_pixel_font.ttf",15)
+            text = font.render("Insufficient gold.",True,(255,255,255))
+            #draw money
+            gameData.screen.blit(text,(700,60))
+
+    #Click on Tower on board:
+    elif gameData.playerSelected==None and inTowerBounds((x,y,x,y)):
+        onBoardDragon = inTowerBounds((x,y,x,y))
+        gameData.playerShowStatus = onBoardDragon
+
+    elif gameData.playerSelected!=None and gameData.playerSelected.onBoard==False:
         #picked up to pokemon to put on board
-        if onBoard(x,y):
+        if onBoard(x,y) and canBuild(x,y):
+            gameData.dragonParty.append(gameData.playerSelected)
+            gameData.playerCoins -= 150 #deduct money
             gameData.playerSelected.x,gameData.playerSelected.y = x,y
             gameData.playerSelected.bounds = x-10,y-10,x+10,y+10
             gameData.playerSelected.onBoard,gameData.playerSelected =True,None
     
-    elif gameData.playerShowStatus!= None and upgradeBound(x,y):
-        (gameData.playerShowStatus).upgradeTower()
+    else:
+        gameData.playerSelected = None
+        gameData.playerShowStatus = None
+
     
 
 
